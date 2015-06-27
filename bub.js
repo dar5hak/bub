28a6d5896bcef67472d1e7b00215638a2b25a1a8
Darshak Parikh <darshak@openmailbox.org>
2015-06-27 19:59:10 +0530

    Ready skeleton

---
+++ bub.js
@@ -0,0 +1,61 @@
+var request = require('hopjs-request');
+
+exports.on = function (command, callback) {
+  // body
+}
+
+exports.getMe = function () {
+  // body
+}
+
+exports.endMessage = function () {
+  // body
+}
+
+exports.forwardMessage = function () {
+  // body
+}
+
+exports.endPhoto = function () {
+  // body
+}
+
+exports.endAudio = function () {
+  // body
+}
+
+exports.endDocument = function () {
+  // body
+}
+
+exports.endSticker = function () {
+  // body
+}
+
+exports.endVideo = function () {
+  // body
+}
+
+exports.endLocation = function () {
+  // body
+}
+
+exports.endChatAction = function () {
+  // body
+}
+
+exports.getUserProfilePhotos = function () {
+  // body
+}
+
+exports.getUpdates = function () {
+  // body
+}
+
+exports.etWebhook = function () {
+  // body
+}
+
+exports.init = function () {
+  // body
+}
